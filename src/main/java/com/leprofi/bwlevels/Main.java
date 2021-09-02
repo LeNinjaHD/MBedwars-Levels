9c8fc295a5f90d2e691e70d3d453b17a70e25676
LeNinjaHD <cedric@leprofi.com>
2021-09-02 17:42:17 +0200

    Added Base Structure

---
+++ src/main/java/com/leprofi/bwlevels/Main.java
@@ -0,0 +1,18 @@
+package com.leprofi.bwlevels;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/*
+ *  Class Created by LeNinjaHD at 02.09.2021
+ */
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
