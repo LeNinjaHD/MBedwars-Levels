1056d9f2880b99079d86582e3acd0d2fded22e15
MetallicGoat <the.great.metallic.goat@gmail.com>
2021-09-02 22:11:51 -0400

    level manager stuff

--- src/main/java/com/leprofi/bwlevels/utils/LevelManager.java
+++ src/main/java/com/leprofi/bwlevels/utils/LevelManager.java
@@ -1,29 +1,37 @@
 package com.leprofi.bwlevels.utils;
 
-import de.marcely.bedwars.api.BedwarsAPI;
+import de.marcely.bedwars.api.player.PlayerDataAPI;
+import de.marcely.bedwars.api.player.PlayerStatSet;
+import de.marcely.bedwars.api.player.PlayerStats;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
+
+import java.util.Optional;
+import java.util.UUID;
 
 /*
  *  Class Created by LeNinjaHD at 02.09.2021
  */
 public class LevelManager {
     private Player player;
+    private final Optional<PlayerStats> playerStats;
+    private final PlayerStatSet expSet = new RegisterMBedwarsStat();
 
-    public LevelManager(String UUID) {
-        player = Bukkit.getPlayer(UUID);
+    public LevelManager(String uuid) {
+        player = Bukkit.getPlayer(uuid);
+        playerStats = PlayerDataAPI.get().getStatsNow(UUID.fromString(uuid));
     }
 
     public void addXP(Integer xp) {
 
+        PlayerDataAPI.get().registerStatSet(expSet);
 
+        playerStats.ifPresent(stats -> expSet.setValue(stats, xp));
     }
 
-    //Don't know if we will need this, and it might be a little difficult to do if level increments are different
-    /*
+
     public void addLevels(Integer levels) {
 
     }
 
-     */
 }
